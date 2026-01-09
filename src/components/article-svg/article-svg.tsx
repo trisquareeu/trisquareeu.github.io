16ee05cafff67d18d38c2ca116ece261f377c0f5
Michał Kalinowski <michal@kalinowski.one>
2026-01-09 21:39:05 +0100

    refactor(blog): move guided generation assets into separate folder

--- src/components/article-svg/article-svg.tsx
+++ src/components/article-svg/article-svg.tsx
@@ -1,5 +1,5 @@
-import ActivationScoresSvg from '../../../public/activation_scores.svg';
-import GrammarsSvg from '../../../public/grammars.svg';
+import ActivationScoresSvg from '../../../public/guided-generation/activation_scores.svg';
+import GrammarsSvg from '../../../public/guided-generation/grammars.svg';
 import './article-svg.css';
 
 export const ArticleSvg = (props: { name: 'grammars' | 'activation_scores' }) => {
